deaee26be89a0e713e5fbab4c8b083759d848f8d
HJRichi <wissens.business@gmail.com>
2024-03-31 18:41:40 +0200

    Die letzte Vrsion war nicht gespeichert. Jetzt gereinigt und mit Kommentaren

--- cachematrix.R
+++ cachematrix.R
@@ -1,44 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Das ist die komplette Funktion zum Speichern und Abrufen der
+## Inversen einer Matrix
 
-## Write a short comment describing this function
-
+# Die erste Funktion, makeCacheMatrix, hat die Aufgabe
+# - den Wert der Matrix zu setzen
+# - den Wert der Matrix abfragen
+# - setzt den Wert der Inversion
+# - liefert den Wert der Inversion
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-
-makeVector <- function(x = numeric()) {
-  m <- NULL
+  ## Initialisiere den Cache für die Inverse
+  inv <- NULL
+  
+  ## Setze die Matrix und lösche den gespeicherten Inversen-Cache
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    inv <<- NULL
   }
+  
+  ## Hole die gesetzte Matrix
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  
+  ## Setze die berechnete Inverse im Cache
+  setinverse <- function(inverse) inv <<- inverse
+  
+  ## Hole die gespeicherte Inverse aus dem Cache
+  getinverse <- function() inv
+  
+  ## Gib eine Liste der Funktionen zurück
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-cachemean <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
+# In dieser Funktion wird die Inversion berechnet, wenn sie nicht im
+# Cache vorhanden ist: inv <- solve(data, ...)
+cacheSolve <- function(x, ...) {
+  ## Zuerst wird versucht, die gespeicherte Inverse aus dem Cache zu holen
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    ## Wenn die Inverse bereits im Cache vorhanden ist, wird eine Nachricht
+    ## ausgegeben und die Inverse zurückgegeben
+    message("getting cached inverse")
+    return(inv)
   }
+  ## Wenn die Inverse nicht im Cache ist, wird die Matrix 'data' aus
+  ## dem 'x' Objekt geholt
   data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
+  ## Dann wird die Inverse der Matrix 'data' mit der Funktion 'solve' berechnet
+  inv <- solve(data, ...)
+  ## Die berechnete Inverse wird im 'x' Objekt gespeichert für
+  ## zukünftige Verwendung
+  x$setinverse(inv)
+  ## Schließlich wird die Inverse zurückgegeben
+  inv
 }
 
-myVector <- makeVector(c(1,2,3,4,5))
-
+myMatrix <- makeCacheMatrix(matrix(data = c(7, 28, 3, 4), nrow = 2))
+cacheSolve(myMatrix) 
+cacheSolve(myMatrix) 
